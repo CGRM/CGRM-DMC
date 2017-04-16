042c10a575135c439ebf8f93cc22da594655e7d5
SeisPider <2013301140017@whu.edu.cn>
2017-04-16 20:21:32 +0800

    Add phase determination method

--- mseed2sac.py
+++ mseed2sac.py
@@ -6,12 +6,12 @@ Scripts used to trim earthquake event waveform from continues waveform.
 miniSEED should be organized as:
 
     |--- YYYYMMDD
-    |    |-- <net>.<sta>.<loc>.<cha>.<starttime>.mseed
-    |    |-- <net>.<sta>.<loc>.<cha>.<starttime>.mseed
+    |    |-- NET.STA.LOC.CHA.STARTTIME.mseed
+    |    |-- NET.STA.LOC.CHA.STARTTIME.mseed
     |    `-- ...
     |--- YYYYMMDD
-    |    |-- <net>.<sta>.<loc>.<cha>.<starttime>.mseed
-    |    |-- <net>.<sta>.<loc>.<cha>.<starttime>.mseed
+    |    |-- NET.STA.LOC.CHA.STARTTIME.mseed
+    |    |-- NET.STA.LOC.CHA.STARTTIME.mseed
     |    `-- ...
     |--- ...
 
@@ -28,14 +28,41 @@ For example:
     |    |-- NET1.STA1.00.BHZ.20160102000000.mseed
     |    `-- ...
     |--- ...
+
+Output SAC files are organized as:
+
+    |--- YYYYMMDDHHMMSS
+    |    |-- YYYY.JDAY.HH.MM.SS.0000.NET.STA.LOC.CHA.M.SAC
+    |    |-- YYYY.JDAY.HH.MM.SS.0000.NET.STA.LOC.CHA.M.SAC
+    |    `-- ...
+    |--- YYYYMMDDHHMMSS
+    |    |-- YYYY.JDAY.HH.MM.SS.0000.NET.STA.LOC.CHA.M.SAC
+    |    |-- YYYY.JDAY.HH.MM.SS.0000.NET.STA.LOC.CHA.M.SAC
+    |    `-- ...
+    |--- ...
+
+For example:
+
+
+    |--- 20160103230522
+    |    |-- 2016.003.23.05.22.0000.AH.ANQ.00.BHE.M.mseed
+    |    |-- 2016.003.23.05.22.0000.AH.ANQ.00.BHN.M.mseed
+    |    |-- 2016.003.23.05.22.0000.AH.ANQ.00.BHZ.M.mseed
+    |    `-- ...
+    |--- 20160105140322
+    |    |-- 2016.005.14.03.22.0000.AH.ANQ.00.BHE.M.mseed
+    |    |-- 2016.005.14.03.22.0000.AH.ANQ.00.BHN.M.mseed
+    |    |-- 2016.005.14.03.22.0000.AH.ANQ.00.BHZ.M.mseed
+    |    `-- ...
 """
 import os
 import logging
 from datetime import timedelta
 
-from obspy import read, UTCDateTime
+from obspy import read, UTCDateTime, Stream
 from obspy.io.sac import SACTrace
-
+from obspy.taup import TauPyModel
+from obspy.geodetics import locations2degrees
 
 # Setup the logger
 FORMAT = "[%(asctime)s]  %(levelname)s: %(message)s"
@@ -48,10 +75,11 @@ logger = logging.getLogger(__name__)
 
 
 class Client(object):
-    def __init__(self, stationinfo, mseeddir, sacdir):
+    def __init__(self, stationinfo, mseeddir, sacdir, model='prem'):
         self.mseeddir = mseeddir
         self.sacdir = sacdir
         self.stations = self._read_stations(stationinfo)
+        self.model = model
 
     def _read_stations(self, stationinfo):
         """
@@ -69,7 +97,7 @@ class Client(object):
                            "stla": float(stla),
                            "stlo": float(stlo),
                            "stel": float(stel),
-                          }
+                           }
                 stations.append(station)
         logger.info("%d stations found.", len(stations))
         return stations
@@ -109,16 +137,30 @@ class Client(object):
             fl_dr_nm0 = os.path.join(self.mseeddir, dirnames[0], pattern)
             fl_dr_nm1 = os.path.join(self.mseeddir, dirnames[1], pattern)
             try:
-                st = read(fl_dr_nm0) + read(fl_dr_nm1)
-            except Exception:
-                logger.error("Error in reading %s", station['name'])
-                return None
+                st = read(fl_dr_nm0)
+            except FileNotFoundError:
+                logger.warning("File not exist: %s ", fl_dr_nm0)
+                st = Stream()
+            except Exception as e:
+                logger.error("Error in reading: %s", e.strerror)
+            try:
+                st += read(fl_dr_nm1)
+            except FileNotFoundError:
+                logger.warning("File not exist: %s ", fl_dr_nm0)
+            except Exception as e:
+                logger.error("Error in reading: %s", e.strerror)
+
         # Merge data
         try:
             st.merge(fill_value=0)
         except Exception:
             logger.error("Error in merging %s", station['name'])
             return None
+        # check if st contains data
+        if not st:
+            logger.warning("No data %s", station['name'])
+            return None
+
         st.trim(starttime, endtime)
         return st
 
@@ -152,41 +194,137 @@ class Client(object):
             sac_trace.evlo = event["longitude"]
             sac_trace.evdp = event["depth"]
             sac_trace.mag = event["magnitude"]
-            # change reference time
-            origin = event["origin"]
-            sac_trace.nzyear = origin.year
-            sac_trace.nzjday = origin.julday
-            sac_trace.nzhour = origin.hour
-            sac_trace.nzmin = origin.minute
-            sac_trace.nzsec = origin.second
-            sac_trace.nzmsec = origin.microsecond / 1000
-            sac_trace.o = 0
+
+            # 1. when change obspy trace to sac trace.
+            #    SACTrace.from_obspy_trace automatically change starttime of
+            #    obspy trace to be reference time of SACTrace
+            #    Thus: sac_trace.b = sac_trace.o = 0.0
+            # 2. Set the sac_trace.o to be event origin, and set the iztype is
+            #    'io' which means the reference time is origin
+            #    Here: These statements not only changed the origin time of sac
+            #    and they automatically changed the b and e
+
+            # 1.from_obspy_trace
+            #   o
+            #   |
+            #   b----------------------e
+            #   |=>   shift  <=|
+            # reftime          |
+            #               origin time
+            #
+            # 2.sac_trace.o = shift
+            #   o:reset to be zero
+            #   |
+            #   b---------------------e
+            #   |            |
+            #   | refer(origin) time
+            # -shift
+
+            sac_trace.o = event["origin"] - sac_trace.reftime
             sac_trace.iztype = 'io'
 
+            sac_trace.lcalda = True
+
             # SAC file location
-            sac_flnm = ".".join([origin.strftime("%Y.%j.%H.%M.%S"),
+            sac_flnm = ".".join([event["origin"].strftime("%Y.%j.%H.%M.%S"),
                                  "0000", trace.id, "M", "SAC"])
             sac_fullname = os.path.join(outdir, sac_flnm)
             sac_trace.write(sac_fullname)
         return
 
-    def get_waveform(self, event, starttime, endtime):
-        dirnames = self._get_dirname(starttime, endtime)
-        logger.debug("dirnames: %s", dirnames)
-        # check if folders exists
-        for dirname in dirnames:
-            if not os.path.exists(os.path.join(self.mseeddir, dirname)):
-                msg = "{} not exist".format(dirname)
-                logger.error(msg)
-                return
+    def _time_determin(self, event, station=None,
+                       event_determ=None, phase_determ=None):
+        """
+        Determin the starttime and endtime
 
+        Parameters
+        ----------
+
+        event: dict
+            Contain information of events
+        station: dict
+            Contain information of station
+        event_dterm: dict
+
+        """
+        if event_determ:
+            starttime = event['origin'] + event_determ['start_offset']
+            endtime = starttime + event_determ['duration']
+            return starttime, endtime
+
+        dist = locations2degrees(event["latitude"], event["longitude"],
+                                 station["stla"], station["stlo"])
+
+        start_ref_phase = phase_determ['start_ref_phase']
+        end_ref_phase = phase_determ['end_ref_phase']
+        start_offset = phase_determ['start_offset']
+        end_offset = phase_determ['end_offset']
+
+        model = TauPyModel(model=self.model)
+        start_arris = model.get_travel_times(source_depth_in_km=event['depth'],
+                                             distance_in_degree=dist,
+                                             phase_list=start_ref_phase)
+        end_arris = model.get_travel_times(source_depth_in_km=event['depth'],
+                                           distance_in_degree=dist,
+                                           phase_list=end_ref_phase)
+        # In start_arris, time are sorted from first arrival to last one
+        start_arrival = [arrival.time for arrival in start_arris][0]
+        end_arrival = [arrival.time for arrival in end_arris][-1]
+
+        # determine starttime and endtime
+        starttime = event['origin'] + start_arrival + start_offset
+        endtime = event['origin'] + end_arrival + end_offset
+        return starttime, endtime
+
+    def get_waveform(self, event, event_determ=None, phase_determ=None):
+        """
+        Trim waveform from dataset of CGRM
+
+        Parameters
+        ----------
+        event: dict
+            Event information container
+        event_determ: dict
+            Determine waveform window with source time
+        phase_determ: dict
+            Determine waveform window with several phases
+
+        """
+        # check the destination
         eventdir = event['origin'].strftime("%Y%m%d%H%M%S")
         outdir = os.path.join(self.sacdir, eventdir)
         if not os.path.exists(outdir):
             os.makedirs(outdir, exist_ok=True)
 
+        if event_determ:
+            starttime, endtime = self._time_determin(event=event,
+                                                     event_determ=event_determ)
+
+            dirnames = self._get_dirname(starttime, endtime)
+            logger.debug("dirnames: %s", dirnames)
+            # check if folders exists
+            for dirname in dirnames:
+                if not os.path.exists(os.path.join(self.mseeddir, dirname)):
+                    msg = "{} not exist".format(dirname)
+                    logger.error(msg)
+                    return
+
         # loop over all stations
         for station in self.stations:
+            if not event_determ:
+                starttime, endtime = self._time_determin(event=event,
+                                                         station=station,
+                                                         phase_determ=phase_determ)
+
+                dirnames = self._get_dirname(starttime, endtime)
+                logger.debug("dirnames: %s", dirnames)
+                # check if folders exists
+                for dirname in dirnames:
+                    if not os.path.exists(os.path.join(self.mseeddir, dirname)):
+                        msg = "{} not exist".format(dirname)
+                        logger.error(msg)
+                        return
+
             st = self._read_mseed(station, dirnames, starttime, endtime)
             # Reading error
             if not st:
@@ -224,11 +362,17 @@ def read_catalog(catalog):
 if __name__ == '__main__':
     client = Client(stationinfo="station.info",
                     mseeddir="MSEED",
-                    sacdir="SAC")
+                    sacdir="SAC",
+                    model="prem")
 
     events = read_catalog("events.csv")
     for event in events:
         logger.info("origin: %s", event['origin'])
-        starttime = event['origin']
-        endtime = starttime + 6000
-        client.get_waveform(event, starttime, endtime)
+        event_determ = {"start_offset": 0, "duration": 6000}
+        phase_determ = {
+            "start_ref_phase": ['P', 'pP'],
+            "start_offset": -100,
+            "end_ref_phase": ['PcP'],
+            "end_offset": 200
+        }
+        client.get_waveform(event, event_determ=event_determ)
