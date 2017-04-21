b121ee13e31d64620dfa45729a24f7dbe5abab6e
Dongdong Tian <seisman.info@gmail.com>
2017-04-21 12:12:48 +0800

    updates

--- mseed2sac.py
+++ mseed2sac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
 """
-Scripts used to trim earthquake event waveform from continues waveform.
+Script to trim earthquake event waveform from continues waveform.
 
 miniSEED should be organized as:
 
@@ -79,7 +79,7 @@ class Client(object):
         self.mseeddir = mseeddir
         self.sacdir = sacdir
         self.stations = self._read_stations(stationinfo)
-        self.model = model
+        self.model = TauPyModel(model=model)
 
     def _read_stations(self, stationinfo):
         """
@@ -96,7 +96,7 @@ class Client(object):
                 station = {"name": name,
                            "stla": float(stla),
                            "stlo": float(stlo),
-                           "stel": float(stel),
+                           "stel": float(stel)
                            }
                 stations.append(station)
         logger.info("%d stations found.", len(stations))
@@ -118,37 +118,25 @@ class Client(object):
 
     def _read_mseed(self, station, dirnames, starttime, endtime):
         """
-        Trim waveform for particular event.
+        Read waveform in specified time window.
         """
-        # return none if dirnames is empty
-        if not dirnames:
+        # obtain event waveform
+        pattern = station['name'] + ".*.*.*.mseed"
+        if not 1 <= len(dirnames) <= 2:  # zero or more than two days
+            logger.error("Cannot trim waveform duration span %s day(s)",
+                         len(dirnames))
             return
 
-        # obtain event waveform
-        pattern = station['name'] + ".*"
-        if len(dirnames) == 1:  # one day
-            fl_dr_nm = os.path.join(self.mseeddir, dirnames[0], pattern)
+        # loop over to read all mseed in
+        st = Stream()
+        for dirname in dirnames:
+            mseedname = os.path.join(self.mseeddir, dirname, pattern)
             try:
-                st = read(fl_dr_nm)
-            except Exception:
-                logger.error("Error in reading %s", station['name'])
-                return None
-        elif len(dirnames) == 2:  # two days
-            fl_dr_nm0 = os.path.join(self.mseeddir, dirnames[0], pattern)
-            fl_dr_nm1 = os.path.join(self.mseeddir, dirnames[1], pattern)
-            try:
-                st = read(fl_dr_nm0)
+                st += read(mseedname)
             except FileNotFoundError:
-                logger.warning("File not exist: %s ", fl_dr_nm0)
-                st = Stream()
+                logger.warning("File not exist: %s", mseedname)
             except Exception as e:
-                logger.error("Error in reading: %s", e.strerror)
-            try:
-                st += read(fl_dr_nm1)
-            except FileNotFoundError:
-                logger.warning("File not exist: %s ", fl_dr_nm0)
-            except Exception as e:
-                logger.error("Error in reading: %s", e.strerror)
+                logger.error("Error in reading: %s", e)
 
         # Merge data
         try:
@@ -156,9 +144,10 @@ class Client(object):
         except Exception:
             logger.error("Error in merging %s", station['name'])
             return None
+
         # check if st contains data
         if not st:
-            logger.warning("No data %s", station['name'])
+            logger.warning("No data for %s", station['name'])
             return None
 
         st.trim(starttime, endtime)
@@ -172,7 +161,7 @@ class Client(object):
             # transfer obspy trace to sac trace
             sac_trace = SACTrace.from_obspy_trace(trace=trace)
 
-            # change some headers about station
+            # set station related headers
             sac_trace.stla = station["stla"]
             sac_trace.stlo = station["stlo"]
             sac_trace.stel = station["stel"]
@@ -189,20 +178,21 @@ class Client(object):
             else:
                 logger.warning("Not E|N|Z component")
 
-            # change some headers about event
+            # set event related headers
             sac_trace.evla = event["latitude"]
             sac_trace.evlo = event["longitude"]
             sac_trace.evdp = event["depth"]
             sac_trace.mag = event["magnitude"]
 
-            # 1. when change obspy trace to sac trace.
-            #    SACTrace.from_obspy_trace automatically change starttime of
-            #    obspy trace to be reference time of SACTrace
-            #    Thus: sac_trace.b = sac_trace.o = 0.0
-            # 2. Set the sac_trace.o to be event origin, and set the iztype is
-            #    'io' which means the reference time is origin
-            #    Here: These statements not only changed the origin time of sac
-            #    and they automatically changed the b and e
+            # 1. SACTrace.from_obspy_trace automatically set Trace starttime
+            #    as the reference time of SACTrace, when converting Trace to
+            #    SACTrace. Thus in SACTrace, b = 0.0.
+            # 2. Set SACTrace.o as the time difference in seconds between
+            #    event origin time and reference time (a.k.a. starttime).
+            # 3. Set SACTrace.iztype to 'io' change the reference time to
+            #    event origin time (determined by SACTrace.o) and also
+            #    automatically change other time-related headers
+            #    (e.g. SACTrace.b).
 
             # 1.from_obspy_trace
             #   o
@@ -219,10 +209,8 @@ class Client(object):
             #   |            |
             #   | refer(origin) time
             # -shift
-
             sac_trace.o = event["origin"] - sac_trace.reftime
             sac_trace.iztype = 'io'
-
             sac_trace.lcalda = True
 
             # SAC file location
@@ -232,8 +220,7 @@ class Client(object):
             sac_trace.write(sac_fullname)
         return
 
-    def _time_determin(self, event, station=None,
-                       event_determ=None, phase_determ=None):
+    def _get_window(self, event, station=None, by_event=None, by_phase=None):
         """
         Determin the starttime and endtime
 
@@ -244,39 +231,40 @@ class Client(object):
             Contain information of events
         station: dict
             Contain information of station
-        event_dterm: dict
-
         """
-        if event_determ:
-            starttime = event['origin'] + event_determ['start_offset']
-            endtime = starttime + event_determ['duration']
+        if by_event:
+            starttime = event['origin'] + by_event['start_offset']
+            endtime = starttime + by_event['duration']
             return starttime, endtime
 
+        # by phase
         dist = locations2degrees(event["latitude"], event["longitude"],
                                  station["stla"], station["stlo"])
 
-        start_ref_phase = phase_determ['start_ref_phase']
-        end_ref_phase = phase_determ['end_ref_phase']
-        start_offset = phase_determ['start_offset']
-        end_offset = phase_determ['end_offset']
+        start_ref_phase = by_phase['start_ref_phase']
+        end_ref_phase = by_phase['end_ref_phase']
+        start_offset = by_phase['start_offset']
+        end_offset = by_phase['end_offset']
 
-        model = TauPyModel(model=self.model)
-        start_arris = model.get_travel_times(source_depth_in_km=event['depth'],
-                                             distance_in_degree=dist,
-                                             phase_list=start_ref_phase)
-        end_arris = model.get_travel_times(source_depth_in_km=event['depth'],
-                                           distance_in_degree=dist,
-                                           phase_list=end_ref_phase)
-        # In start_arris, time are sorted from first arrival to last one
-        start_arrival = [arrival.time for arrival in start_arris][0]
-        end_arrival = [arrival.time for arrival in end_arris][-1]
+        # TauPyModel.get_travel_times always return sorted value
+        start_arrivals = model.get_travel_times(
+            source_depth_in_km=event['depth'],
+            distance_in_degree=dist,
+            phase_list=start_ref_phase)
+        if not start_arrivals:  # no phase avaiable, skip this data
+            return None, None  # starttime and endtime are None
+
+        end_arrivals = model.get_travel_times(
+            source_depth_in_km=event['depth'],
+            distance_in_degree=dist,
+            phase_list=end_ref_phase)
 
         # determine starttime and endtime
-        starttime = event['origin'] + start_arrival + start_offset
-        endtime = event['origin'] + end_arrival + end_offset
+        starttime = event['origin'] + start_arrivals[0].time + start_offset
+        endtime = event['origin'] + end_arrivals[-1].time + end_offset
         return starttime, endtime
 
-    def get_waveform(self, event, event_determ=None, phase_determ=None):
+    def get_waveform(self, event, by_event=None, by_phase=None):
         """
         Trim waveform from dataset of CGRM
 
@@ -284,11 +272,10 @@ class Client(object):
         ----------
         event: dict
             Event information container
-        event_determ: dict
-            Determine waveform window with source time
-        phase_determ: dict
-            Determine waveform window with several phases
-
+        by_event: dict
+            Determine waveform window by event origin time
+        by_phase: dict
+            Determine waveform window by phase arrival times
         """
         # check the destination
         eventdir = event['origin'].strftime("%Y%m%d%H%M%S")
@@ -296,37 +283,23 @@ class Client(object):
         if not os.path.exists(outdir):
             os.makedirs(outdir, exist_ok=True)
 
-        if event_determ:
-            starttime, endtime = self._time_determin(event=event,
-                                                     event_determ=event_determ)
-
+        if by_event:
+            starttime, endtime = self._get_window(event=event,
+                                                  by_event=by_event)
             dirnames = self._get_dirname(starttime, endtime)
             logger.debug("dirnames: %s", dirnames)
-            # check if folders exists
-            for dirname in dirnames:
-                if not os.path.exists(os.path.join(self.mseeddir, dirname)):
-                    msg = "{} not exist".format(dirname)
-                    logger.error(msg)
-                    return
 
         # loop over all stations
         for station in self.stations:
-            if not event_determ:
-                starttime, endtime = self._time_determin(event=event,
-                                                         station=station,
-                                                         phase_determ=phase_determ)
-
+            logger.debug("station: %s", station['name'])
+            if not by_event:
+                starttime, endtime = self._get_window(event=event,
+                                                      station=station,
+                                                      by_phase=by_phase)
                 dirnames = self._get_dirname(starttime, endtime)
                 logger.debug("dirnames: %s", dirnames)
-                # check if folders exists
-                for dirname in dirnames:
-                    if not os.path.exists(os.path.join(self.mseeddir, dirname)):
-                        msg = "{} not exist".format(dirname)
-                        logger.error(msg)
-                        return
 
             st = self._read_mseed(station, dirnames, starttime, endtime)
-            # Reading error
             if not st:
                 continue
             self._writesac(st, event, station, outdir)
@@ -368,11 +341,11 @@ if __name__ == '__main__':
     events = read_catalog("events.csv")
     for event in events:
         logger.info("origin: %s", event['origin'])
-        event_determ = {"start_offset": 0, "duration": 6000}
-        phase_determ = {
-            "start_ref_phase": ['P', 'pP'],
+        by_event = {"start_offset": 0, "duration": 6000}
+        by_phase = {
+            "start_ref_phase": ['P', 'p'],
             "start_offset": -100,
             "end_ref_phase": ['PcP'],
             "end_offset": 200
         }
-        client.get_waveform(event, event_determ=event_determ)
+        client.get_waveform(event, by_event=by_event)
